aa47d164acff13e130f18c1fda0f2a1d83a34ea4
Rendra Suroso <rs@cogsci.bandungfe.net>
2018-08-08 18:37:35 +0700

    Refactor tests.

--- tests/SMTPStoreTest.php
+++ tests/SMTPStoreTest.php
@@ -5,10 +5,11 @@ use PHPUnit\Framework\TestCase;
 use BFITech\ZapCore\Logger;
 use BFITech\ZapCommonDev\CommonDev;
 use BFITech\ZapCoreDev\RouterDev;
+use BFITech\ZapCoreDev\RoutingDev;
 use BFITech\ZapStore\SQLite3;
 use BFITech\ZapAdmin\SMTPRoute;
-use BFITech\ZapAdmin\AdminStoreError;
-use BFITech\ZapAdmin\SMTPRouteError as Err;
+use BFITech\ZapAdmin\AdminStoreError as AErr;
+use BFITech\ZapAdmin\SMTPRouteError as SErr;
 
 
 class SMTPFixture {
@@ -85,11 +86,6 @@ class SMTPRouteTest extends TestCase {
 		$smtp->smtp_add_service($host, $port, $ssl, $timeout, $opts);
 	}
 
-	private function core_reinit($core) {
-		$core->deinit()->reset();
-		$core->config('home', '/');
-	}
-
 	public function test_connection() {
 		$smtp = $this->make_smtp();
 
@@ -98,18 +94,18 @@ class SMTPRouteTest extends TestCase {
 		$this->assertEquals(0,
 			$smtp->smtp_add_service($host, $port));
 		# cannot re-add existing service
-		$this->assertEquals(Err::SRV_ADD_FAILED,
+		$this->assertEquals(SErr::SRV_ADD_FAILED,
 			$smtp->smtp_add_service($host, $port));
 
 		$this->assertSame($smtp->smtp_list_services()[0],
 			[$host, $port]);
 
 		# connect to wrong port
-		$this->assertEquals(Err::SRV_NOT_FOUND,
+		$this->assertEquals(SErr::SRV_NOT_FOUND,
 			$smtp->smtp_connect($host, $port + 1));
 
 		# fail opening connection
-		$this->assertEquals(Err::CONNECT_FAILED,
+		$this->assertEquals(SErr::CONNECT_FAILED,
 			$smtp->smtp_connect($host, $port));
 
 	}
@@ -120,14 +116,16 @@ class SMTPRouteTest extends TestCase {
 		$acc_bogus = $conf['bogus'];
 		$acc_valid = $conf['valid'];
 
-		if ($acc_valid['host'] === null)
+		if ($acc_valid['host'] === null) {
+			$this->markTestIncomplete('Valid host not provided.');
 			return;
+		}
 
 		$smtp = $this->make_smtp();
 
 		$smtp->smtp_add_service($acc_valid['host'], $acc_valid['port']);
 
-		$this->assertEquals(Err::NOT_CONNECTED,
+		$this->assertEquals(SErr::NOT_CONNECTED,
 			$smtp->smtp_authenticate(
 				$acc_bogus['username'], $acc_bogus['password']));
 
@@ -138,7 +136,7 @@ class SMTPRouteTest extends TestCase {
 		# connect again, will internally reconnect
 		$smtp->smtp_connect($acc_valid['host'], $acc_valid['port']);
 
-		$this->assertEquals(Err::AUTH_FAILED,
+		$this->assertEquals(SErr::AUTH_FAILED,
 			$smtp->smtp_authenticate(
 				$acc_bogus['username'], $acc_bogus['password']));
 
@@ -153,6 +151,7 @@ class SMTPRouteTest extends TestCase {
 	public function test_list_service() {
 		$smtp = $this->make_smtp();
 		$core = $smtp->core;
+		$rdev = new RoutingDev($core);
 
 		$conf = [];
 		$i = 0;
@@ -162,7 +161,7 @@ class SMTPRouteTest extends TestCase {
 				'port' => 3587 + $i++,
 			]);
 
-		$_SERVER['REQUEST_URI'] = '/smtp/srv';
+		$rdev->request('/smtp/srv');
 		$smtp->route('/smtp/srv', [$smtp, 'route_smtp_list']);
 		$this->assertEquals($core::$errno, 0);
 		$this->assertSame($core::$data[0][0], 'localhost');
@@ -170,71 +169,64 @@ class SMTPRouteTest extends TestCase {
 		$this->assertSame($core::$data[1][1], 3588);
 	}
 
-	public function test_route() {
-		$conf = SMTPFixture::$config;
-		$acc_bogus = $conf['bogus'];
-		$acc_valid = $conf['valid'];
+	public function test_route_bogus() {
+		$acc = SMTPFixture::$config['bogus'];
 
 		$smtp = $this->make_smtp();
 		$core = $smtp->core;
-		$this->make_service($smtp, $acc_bogus);
-		$this->make_service($smtp, $acc_valid);
+		$rdev = new RoutingDev($core);
+		$this->make_service($smtp, $acc);
 
 		# incomplete data
 
-		$_SERVER['REQUEST_URI'] = '/smtp/auth';
-		$_SERVER['REQUEST_METHOD'] = 'POST';
-		$_POST = [
-			'smtp_host' => $acc_bogus['host'],
-			'smtp_port' => $acc_bogus['port'],
-			'username' => $acc_bogus['username'],
+		$post = [
+			'smtp_host' => $acc['host'],
+			'smtp_port' => $acc['port'],
+			'username' => $acc['username'],
 		];
+		$rdev->request('/smtp/auth', 'POST', ['post' => $post]);
 		$smtp->route('/smtp/auth', [$smtp, 'route_smtp_auth'], 'POST');
-		$this->assertEquals($core::$errno, Err::AUTH_INCOMPLETE_DATA);
-		$this->core_reinit($core);
+		$this->assertEquals($core::$errno, SErr::AUTH_INCOMPLETE_DATA);
 
 		# service down or invalid
 
-		$_SERVER['REQUEST_URI'] = '/smtp/auth';
-		$_SERVER['REQUEST_METHOD'] = 'POST';
-		$_POST = [
-			'smtp_host' => $acc_bogus['host'],
-			'smtp_port' => $acc_bogus['port'],
-			'username' => $acc_bogus['username'],
-			'password' => $acc_bogus['password'],
-		];
+		$post['password'] = $acc['password'];
+		$rdev->request('/smtp/auth', 'POST', ['post' => $post]);
 		$smtp->route('/smtp/auth', [$smtp, 'route_smtp_auth'], 'POST');
-		$this->assertEquals($core::$errno, Err::CONNECT_FAILED);
-		$this->core_reinit($core);
+		$this->assertEquals($core::$errno, SErr::CONNECT_FAILED);
+
+	}
+
+	public function test_route_valid() {
+		$acc = SMTPFixture::$config['valid'];
 
 		# only run if there's a live server
-		if ($acc_valid['host'] == null)
+		if ($acc['host'] == null) {
+			$this->markTestIncomplete('Valid host not provided.');
 			return;
+		}
+
+		$smtp = $this->make_smtp();
+		$core = $smtp->core;
+		$rdev = new RoutingDev($core);
+		$this->make_service($smtp, $acc);
 
 		# wrong password
 
-		$_SERVER['REQUEST_URI'] = '/smtp/auth';
-		$_SERVER['REQUEST_METHOD'] = 'POST';
-		$_POST = [
-			'smtp_host' => $acc_valid['host'],
-			'smtp_port' => $acc_valid['port'],
-			'username' => $acc_bogus['username'],
-			'password' => $acc_bogus['password'],
+		$post = [
+			'smtp_host' => $acc['host'],
+			'smtp_port' => $acc['port'],
+			'username' => $acc['username'],
+			'password' => $acc['password'] . 'xxxx',
 		];
+		$rdev->request('/smtp/auth', 'POST', ['post' => $post]);
 		$smtp->route('/smtp/auth', [$smtp, 'route_smtp_auth'], 'POST');
-		$this->assertEquals($core::$errno, Err::AUTH_FAILED);
-		$this->core_reinit($core);
+		$this->assertEquals($core::$errno, SErr::AUTH_FAILED);
 
 		# success
 
-		$_SERVER['REQUEST_URI'] = '/smtp/auth';
-		$_SERVER['REQUEST_METHOD'] = 'POST';
-		$_POST = [
-			'smtp_host' => $acc_valid['host'],
-			'smtp_port' => $acc_valid['port'],
-			'username' => $acc_valid['username'],
-			'password' => $acc_valid['password'],
-		];
+		$post['password'] = $acc['password'];
+		$rdev->request('/smtp/auth', 'POST', ['post' => $post]);
 		$smtp->route('/smtp/auth', [$smtp, 'route_smtp_auth'], 'POST');
 		$this->assertEquals($core::$errno, 0);
 		$data = $core::$data;
@@ -242,27 +234,19 @@ class SMTPRouteTest extends TestCase {
 		# test status
 
 		$uname = sprintf('+%s:smtp[%s:%s]',
-			rawurlencode($_POST['username']),
-			$_POST['smtp_host'], $_POST['smtp_port']);
+			rawurlencode($post['username']),
+			$post['smtp_host'], $post['smtp_port']);
 		$smtp->adm_set_user_token($data['token']);
 		$rv = $smtp->adm_status();
 		$this->assertEquals($rv['uname'], $uname);
-		$this->core_reinit($core);
 
 		# resign-in will fail
 
-		$_SERVER['REQUEST_URI'] = '/smtp/auth';
-		$_SERVER['REQUEST_METHOD'] = 'POST';
-		$_POST = [
-			'smtp_host' => $acc_valid['host'],
-			'smtp_port' => $acc_valid['port'],
-			'username' => $acc_valid['username'],
-			'password' => $acc_valid['password'],
-		];
+		$rdev->request('/smtp/auth', 'POST', ['post' => $post]);
 		$smtp->route('/smtp/auth', [$smtp, 'route_smtp_auth'], 'POST');
 		$this->assertEquals($core::$code, 401);
 		$this->assertEquals($core::$errno,
-			AdminStoreError::USER_ALREADY_LOGGED_IN);
+			AErr::USER_ALREADY_LOGGED_IN);
 	}
 
 }
